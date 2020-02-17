4be6b8464fa56fbf1ef4d132b947135ec7c5c0db
Smeelock <aurelien.vungoc@gmail.com>
2020-02-17 16:12:52 +0100

    ant.py

--- Ant.py
+++ Ant.py
@@ -1,5 +1,24 @@
 class Ant:
     def __init__(self):
+        self.__alpha = 0 
+        self.__beta = 0
+        self.__gamma = 0
+        self.__carry_food = False
+        self.__stepCapacity = 1 # default step capacity : takes 1 step at a time
+ 
+    def getTrend(self, pheroLevel):
+        """ According to pheromon level (float), chooses the best route to move forward towards objective """
         pass
+
+    def catchFood(self):
+        pass
+
+    def leaveFood(self):
+        pass
+
+    def walk(self):
+        pass
+
+
 
         
