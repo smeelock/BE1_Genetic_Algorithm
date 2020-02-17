d6d85948ec06ed77a53ee4f31d5ff60e8700ba3c
Smeelock <aurelien.vungoc@gmail.com>
2020-02-17 17:08:12 +0100

    adding pheromon evaporation + spreading

--- Ant.py
+++ Ant.py
@@ -1,15 +1,24 @@
+import Route
+import math
+import random
+
 class Ant:
-    def __init__(self):
-        self.__alpha = 0 
-        self.__beta = 0
-        self.__gamma = 0
+    def __init__(self, initial_city):
+        self.__alpha = random.uniform(-5, 5)
+        self.__beta = random.uniform(-5, 5)
+        self.__gamma = random.uniform(-5, 5)
         self.__carry_food = False
 
-        self.__x = 0
-        self.__y = 0
-        self.__stepCapacity = 1 # default step capacity : takes 1 step at a time
-        self.__visitedCity = [] # queue: to remember where the ant went
- 
+        self.__current_city = initial_city
+
+        self.__current_route = None
+        self.__remaning_steps_on_current_route = None
+        self.__currently_on_the_road = (self.__current_route != None)
+
+        self.__step_capacity = 1 # default step capacity : takes 1 step at a time
+        self.__routes_taken = [] # queue: to remember where the ant went (lifo)
+                                    # because when arriving to food, wants to go home so taking the last route (not the most ancient one)
+    
     def getTrend(self, pheroLevel):
         """ According to pheromon level (float), chooses the best route to move forward towards objective """
         pass
@@ -24,7 +33,14 @@ class Ant:
         pass
 
     def spreadPheronom(self): # when walking on edges, ants leaves pheromon where they go
-        pass
+        pl = self.__current_route.getPheromonLevel()
+        pl = self.__alpha*math.sin(self.__beta*pl + self.__gamma)
+        self.__current_route.updatePheromonLevel(pl)
+
+    def takeRoute(self, route):
+        self.__current_route = route
+        self.__remaning_steps_on_current_route = route.getLength()
+        self.__routes_taken.append(route)
 
 
 
--- Route.py
+++ Route.py
@@ -1,14 +1,23 @@
 import City
 
+RHO = 0.5 # evaporation ratio
+
 class Route:
     def __init__(self, start_city, end_city):
         self.__start_city = start_city
         self.__end_city = end_city
-        self.__pheromon_quantity = 0
+        self.__pheromon_level = 0
         self.__length = 0
     
     def evaporationOfPheromon(self):
-        pass
+        self.__pheromon_level *= (1 - RHO)
+    
+    def updatePheromonLevel(self, new_phero_level):
+        self.__pheromon_level = new_phero_level
+    
+    def getPheromonLevel(self):
+        return self.__pheromon_level
+
 
     def getLength(self):
         return self.__length
